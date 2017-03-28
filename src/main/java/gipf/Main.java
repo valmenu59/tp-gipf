d19fe4916c7a75b5a659fda102aa4d1b69046bb7
Julien Vion <julien.vion@univ-valenciennes.fr>
2017-03-28 13:55:58 +0200

    Amélioration du code

--- src/main/java/gipf/Main.java
+++ src/main/java/gipf/Main.java
@@ -6,7 +6,6 @@ import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.Random;
@@ -17,6 +16,76 @@ import org.postgresql.ds.PGSimpleDataSource;
 public class Main {
 	private static final Random RAND = new Random(0);
 
+	/**
+	 * Suppression de toutes les données
+	 * 
+	 * @param con
+	 * @throws SQLException
+	 */
+	private static void clean(Connection con) throws SQLException {
+		try (Statement stmt = con.createStatement()) {
+			stmt.executeUpdate("TRUNCATE TABLE Partie, Arbitre, Tournoi, Joueur");
+		}
+	}
+
+	/**
+	 * Inscription d'une liste de joueurs avec mot de passe aléatoire
+	 * 
+	 * (Question 2)
+	 * 
+	 * @param usernames
+	 * @param con
+	 * @return
+	 * @throws SQLException
+	 */
+	private static List<Joueur> inscrire(List<String> usernames, Connection con) throws SQLException {
+		List<Joueur> joueurs = new ArrayList<>();
+		for (String username : usernames) {
+			joueurs.add(
+					Joueur.inscrire(username, UUID.randomUUID().toString(), username + "@univ-valenciennes.fr", con));
+		}
+		return joueurs;
+	}
+
+	/**
+	 * Affichage du classement ELO (Question 4)
+	 * 
+	 * @param con
+	 * @throws SQLException
+	 */
+	private static void classementElo(Connection con) throws SQLException {
+		for (Joueur j : Joueur.loadByElo(con)) {
+			System.out.println(j);
+		}
+		System.out.println();
+	}
+
+	/**
+	 * Affichage du classement par nombre de parties gagnées (Question 5)
+	 * 
+	 * @param con
+	 * @throws SQLException
+	 */
+	private static void classementPartiesGagnees(Connection con) throws SQLException {
+		for (Entry<Joueur, Integer> j : Joueur.loadByPartiesGagnees(con).entrySet()) {
+			System.out.println(j);
+		}
+		System.out.println();
+	}
+
+	/**
+	 * Affichage du classement par nombre de parties jouées (Question 8)
+	 * 
+	 * @param con
+	 * @throws SQLException
+	 */
+	private static void classementPartiesJouees(Connection con) throws SQLException {
+		for (Entry<Joueur, Integer> j : Joueur.loadByPartiesJouees(con).entrySet()) {
+			System.out.println(j);
+		}
+		System.out.println();
+	}
+
 	public static void main(String[] args) throws SQLException {
 		PGSimpleDataSource ds = new PGSimpleDataSource();
 		ds.setServerName("localhost");
@@ -24,19 +93,11 @@ public class Main {
 
 		try (Connection con = ds.getConnection("vion", "vion")) {
 
-			try (Statement stmt = con.createStatement()) {
-				// Suppression de toutes les données
-				stmt.executeUpdate("TRUNCATE TABLE Partie, Arbitre, Tournoi, Joueur");
-			}
+			clean(con);
 
-			// Inscription de quelques joueurs avec mot de passe aléatoire
-			// (Question 2)
-			List<Joueur> joueurs = new ArrayList<>();
-			for (String username : Arrays.asList("baroqueen", "cobrag", "vikingkong", "preaster", "fickleSkeleton",
-					"SnowTea", "AfternoonTerror", "JokeCherry", "JealousPelican", "PositiveLamb")) {
-				joueurs.add(Joueur.inscrire(username, UUID.randomUUID().toString(), username + "@univ-valenciennes.fr",
-						con));
-			}
+			List<Joueur> joueurs = inscrire(Arrays.asList("baroqueen", "cobrag", "vikingkong", "preaster",
+					"fickleSkeleton", "SnowTea", "AfternoonTerror", "JokeCherry", "JealousPelican", "PositiveLamb"),
+					con);
 
 			// Enregistrement de quelques parties (Question 3)
 			List<Partie> parties = new ArrayList<>();
@@ -56,20 +117,13 @@ public class Main {
 
 			// Affichages
 			System.out.println("*** Classement ELO (Question 4) ***");
-			for (Joueur j : Joueur.loadByElo(con)) {
-				System.out.println(j);
-			}
-			System.out.println();
+			classementElo(con);
 
 			System.out.println("*** Classement parties gagnées (Question 5) ***");
-			for (Entry<Joueur, Integer> j : Joueur.loadByPartiesGagnees(con).entrySet()) {
-				System.out.println(j);
-			}
-			System.out.println();
+			classementPartiesGagnees(con);
 
 			// Enregistrement d'un tournoi (Question 6)
-			// Sélection aléatoire de trois arbitres dans la liste des joueurs
-			Collections.shuffle(joueurs, RAND);
+			// Sélection de trois arbitres dans la liste des joueurs
 			List<Joueur> arbitres = joueurs.subList(0, 3);
 
 			Tournoi t = Tournoi.create(LocalDate.of(2017, 3, 10), LocalDate.of(2017, 3, 12), "Maubeuge", arbitres, con);
@@ -82,11 +136,7 @@ public class Main {
 			}
 
 			System.out.println("*** Classement parties jouées (Question 8) ***");
-			for (Entry<Joueur, Integer> j : Joueur.loadByPartiesJouees(con).entrySet()) {
-				System.out.println(j);
-			}
-			System.out.println();
-
+			classementPartiesJouees(con);
 		}
 	}
 }
