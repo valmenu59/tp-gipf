59269b22dde29feeed33792a6e6abef91516181e
Julien Vion <julien.vion@univ-valenciennes.fr>
2017-03-31 11:55:17 +0200

    Suppression du code SQL pour l'exercice

--- src/main/sql/create.sql
+++ src/main/sql/create.sql
@@ -1,34 +1,3 @@
-DROP TABLE IF EXISTS Arbitre, Partie, Tournoi, Joueur;
+-- DROP TABLE IF EXISTS ...
 
--- Modèle relationnel (Question 1)
-
-CREATE TABLE Joueur (
-  login text PRIMARY KEY,
-  elo real NOT NULL DEFAULT 1000,
-  password text NOT NULL,
-  email text NOT NULL UNIQUE CHECK (email ~ '@'));
-
-CREATE TABLE Tournoi (
-  idTournoi serial primary key,
-  dateDebut date not null default now(),
-  dateFin date,
-  check (dateFin >= dateDebut),
-  lieu text not null);
-
-CREATE TABLE Partie (
-  idPartie serial primary key,
-  datePartie timestamp not null default now(),
-  piecesRestantes int check (piecesRestantes >= 0),
-  blanc text not null references Joueur,
-  noir text not null references Joueur,
-  gagnant text references Joueur,
-  perdant text references Joueur,
-  idTournoi int references Tournoi,
-  check (blanc != noir),
-  check (gagnant = noir or perdant = noir),
-  check (gagnant = blanc or perdant = blanc));
-
-CREATE TABLE Arbitre (
-  login text references Joueur,
-  idTournoi int references Tournoi,
-  primary key (login, idTournoi));
+-- Modèle relationnel 
